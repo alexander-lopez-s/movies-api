e19ce7e2174dd5f05bffff8725770a6c51192d95
Cristobal Alexander Lopez Segovia <alxlpz@outlook.com>
2023-09-22 11:02:20 +0200

    state updated

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,12 @@
+import { useState } from 'react';
 
 const MovieCard = ({ movieData }) => {
+  const [liked, setLiked] = useState(false); 
+
+  const toggleLike = () => {
+    setLiked(!liked); 
+  };
+
   return (
     <div className="movie-card">
       <img className="movie-poster" src={movieData.Poster} alt={`${movieData.Title} Poster`} />
@@ -8,6 +15,7 @@ const MovieCard = ({ movieData }) => {
         <p className="movie-year"><strong>Year: </strong>{movieData.Year}</p>
         <p className="movie-genre"><strong>Genre:</strong> {movieData.Genre}</p>
         <p className="movie-plot"><strong>Plot:</strong> {movieData.Plot}</p>
+        <span className={`heart-icon ${liked ? 'liked' : ''}`} onClick={toggleLike}><p>♥</p></span>
       </div>
     </div>
   );
--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -1,20 +1,21 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import "../App.CSS";
+import '../App.css';
 import MovieCard from './MovieCard';
 
-const OMDB_API_KEY = 'd024085f';
+const API_KEY = 'd024085f';
 
 function MovieSearch() {
   const [searchTerm, setSearchTerm] = useState('');
   const [movieData, setMovieData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [newSearch, setNewSearch] = useState(false);
 
   useEffect(() => {
     const fetchRandomMovie = async () => {
       try {
-        const response = await axios.get(`https://www.omdbapi.com/?t=The+Conjuring&apikey=${OMDB_API_KEY}`);
+        const response = await axios.get(`https://www.omdbapi.com/?t=The+Conjuring&apikey=${API_KEY}`);
         setMovieData(response.data);
       } catch (error) {
         console.error('Error fetching movie data:', error);
@@ -27,17 +28,16 @@ function MovieSearch() {
     fetchRandomMovie();
   }, []);
 
-  console.log(movieData);
-
   const handleSearch = async () => {
     try {
-      const response = await axios.get(`https://www.omdbapi.com/?t=${searchTerm}&apikey=${OMDB_API_KEY}`);
-      if (response.data.Response === "False") {
+      const response = await axios.get(`https://www.omdbapi.com/?t=${searchTerm}&apikey=${API_KEY}`);
+      if (response.data.Response === 'False') {
         setError('Movie not found!');
         setMovieData(null);
       } else {
         setMovieData(response.data);
-        setError(null); 
+        setError(null);
+        setNewSearch(true);
       }
     } catch (error) {
       console.error('Error fetching movie data:', error);
@@ -63,21 +63,20 @@ function MovieSearch() {
         onKeyPress={handleKeyPress}
       />
       <button onClick={handleSearch}>Search</button>
-  
+
       {error && (
         <div className="movie-card">
           <p className="error-message">{error}</p>
         </div>
       )}
-  
+
       {loading ? (
         <p>Loading...</p>
-      ) : movieData ? ( 
-        <MovieCard movieData={movieData} />
+      ) : movieData ? (
+        <MovieCard movieData={movieData} key={newSearch} />
       ) : null}
     </div>
   );
-  
 }
 
 export default MovieSearch;
